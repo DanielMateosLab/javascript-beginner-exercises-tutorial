48aaeeaf4aacb3bff6b7a4754ecf72dc7538b1cb
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-12-10 22:29:27 +0000

    First attempt to solve 24

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,34 @@
 // Your code here:
+for (let i = 99; i == 0; i--) {
+    console.log(getFirstVerse(1) + getSecondVerse(i))
+}
+
+function getFirstVerse(counter) {
+    let currentBottlesString = getBottlesString(counter)
+
+    return `${capitalize(currentBottlesString)} of milk on the wall, ${currentBottlesString} of milk. `
+}
+
+function getSecondVerse(counter) {
+    let remainingBottles = counter - 1
+    let firstSentence = "Take one down and pass it around"
+
+    if (counter == 0) {
+        firstSentence = "Go to the store and buy some more"
+        remainingBottles = 99
+    }
+
+    return `${firstSentence}, ${getBottlesString(remainingBottles)} of milk on the wall.`
+}
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function getBottlesString(bottlesNumber) {
+    return (bottlesNumber > 0 ? String(bottlesNumber) : "no more") + " bottle" + getFinalChar(bottlesNumber)
+}
+
+function getFinalChar(n) {
+    return n == 1 ? "" : "s"
+}
